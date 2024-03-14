657d8043a0158bf3c29a25119872da3fdf0177b6
huycuongngo <cuong.nh@softenmind.com>
2024-03-14 19:10:01 +0700

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 function Header() {
   return (
     <div style={{
-      padding: '10px 20px',
+      padding: '10px 50px',
       backgroundColor: '#F8E1DB'
     }}>
       <img src="/imgs/logoicon.png" alt="" />
-      <div>
+      <div style={{
+        display: 'flex',
+        gap: 30
+      }}>
         <p>Home</p>
         <p>Search</p>
         <p>Destination</p>
